8b16f701af55f058313bad705915ca39e18ac165
Blaze-0903 <sujal.junghare.btech2022@sitnagpur.siu.edu.in>
2023-05-28 13:10:17 +0530

    Confirmation popup

--- saarthi.js
+++ saarthi.js
@@ -43,6 +43,5 @@ document.addEventListener("DOMContentLoaded", function() {
   let popup=document.getElementById("popup");
   function openPopup(){
     popup.classList.add("open-popup");
-    
   }
   
